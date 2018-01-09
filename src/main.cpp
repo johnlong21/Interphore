e61a6ba55d9b63826e969923d93cd86b3d372ea4
FallowWing <fallowwing@gmail.com>
2018-01-09 13:49:24 -0700

    Clean and build

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 #define VERSION "0.0.1"
 
 /// Game
-#define ACTION_QUEUE_LIMIT 128
-#define OVERLAP_LIMIT 64
-#define CHOICE_LABEL_LIMIT 128
-#define MAP_OBJECT_LIMIT 512
-#define DOOR_FROM_LIMIT 32
+// nil
 
 /// Debugging
 // #define LOG_ASSET_ADD
@@ -22,17 +18,9 @@
 
 #include "gameEngine.h"
 
-#include "game.cpp"
-#include "hotspot.cpp"
-#include "level.cpp"
-
 extern "C" void entryPoint();
 void mainUpdate();
 void mainInit();
-
-bool passedSitelock = false;
-bool isTeaser = false;
-bool pressed[4] = {};
 
 #ifdef SEMI_WIN32
 INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
@@ -50,43 +38,9 @@ void entryPoint() {
 }
 
 void mainInit() {
-// #ifdef SITELOCK
-// 	passedSitelock = false;
-// #elif defined(SEMI_EARLY)
-// 	isTeaser = true;
-
-// 	MintSprite *bg = createMintSprite();
-// 	bg->setupRect(1, 1, 0);
-// 	bg->scale(engine->width, engine->height);
-// 	bg->scrolls = false;
-// 	bg->layer = 0;
-
-// 	strcpy(engine->spriteData.defaultFont, "Espresso-Dolce_50");
-// 	MintSprite *text = createMintSprite();
-// 	text->setupEmpty(200, 200);
-// 	text->setText("Soon.");
-// 	text->x = engine->width/2 - text->textWidth/2;
-// 	text->y = engine->height/2 - text->textHeight/2;
-// #else
-//	passedSitelock = true;
-//	initGame();
-// #endif
-	initGame();
+	printf("Init\n");
 }
 
 void mainUpdate() {
-		updateGame();
-	// if (isTeaser) return;
-	// if (passedSitelock) {
-	// 	updateGame();
-	// } else {
-	// 	if (keyIsJustPressed('P')) pressed[0] = true;
-	// 	if (keyIsJustPressed('L')) pressed[1] = true;
-	// 	if (keyIsJustPressed('A')) pressed[2] = true;
-	// 	if (keyIsJustPressed('Y')) pressed[3] = true;
-	// 	if (pressed[0] && pressed[1] && pressed[2] && pressed[3]) {
-	// 		initGame();
-	// 		passedSitelock = true;
-	// 	}
-	// }
+	printf("Update\n");
 }
