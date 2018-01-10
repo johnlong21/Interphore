0637ce8fb12f769703bc2309544552d7292c51c8
FallowWing <fallowwing@gmail.com>
2018-01-10 12:48:14 -0700

    Added a title and button, also started a state thing

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,20 @@ void mainUpdate();
 void mainInit();
 void js_print(CScriptVar *v, void *userdata);
 
+enum GameState { STATE_NULL=0, STATE_MENU, STATE_MOD };
+
+struct GameStruct {
+	MintSprite *bg;
+	MintSprite *title;
+	MintSprite *subtitle;
+
+	MintSprite *loadButton;
+	MintSprite *loadButtonTf;
+
+	GameState state;
+};
+
+GameStruct *game;
 CTinyJS *jsInterp;
 
 #ifdef SEMI_WIN32
@@ -47,6 +61,56 @@ void mainInit() {
 	registerFunctions(jsInterp);
 	jsInterp->addNative("function print(text)", &js_print, 0);
 	jsInterp->execute("print(\"This is a print test in js\");");
+	strcpy(engine->spriteData.defaultFont, "OpenSans-Regular_20");
+
+	game = (GameStruct *)zalloc(sizeof(GameStruct));
+
+	{ /// Bg
+		MintSprite *spr = createMintSprite();
+		spr->setupRect(engine->width, engine->height, 0x000000);
+
+		game->bg = spr;
+	}
+
+	{ /// Title
+		MintSprite *spr = createMintSprite();
+		spr->setupEmpty(engine->width, 100);
+		spr->setText("Writer");
+		spr->x = engine->width/2 - spr->getWidth()/2;
+
+		game->title = spr;
+	}
+
+	{ /// Subtitle
+		MintSprite *spr = createMintSprite();
+		spr->setupEmpty(engine->width, 100);
+		spr->setText("A story tool");
+
+		game->title->addChild(spr);
+		spr->gravitate(0.5, 0.5);
+		spr->y = spr->parent->getHeight() + 10;
+
+		game->subtitle = spr;
+	}
+
+	{ /// Load button
+		MintSprite *spr = createMintSprite();
+		spr->setupRect(128, 64, 0x444444);
+		spr->x = engine->width/2 - spr->width/2;
+		spr->y = engine->height - spr->getHeight() - 10;
+
+		game->loadButton = spr;
+	}
+
+	{ /// Load button text
+		MintSprite *spr = createMintSprite();
+		spr->setupEmpty(game->loadButton->getFrameWidth(), game->loadButton->getFrameHeight());
+		spr->setText("Load");
+		game->loadButton->addChild(spr);
+		spr->gravitate(0.5, 0.5);
+
+		game->loadButtonTf = spr;
+	}
 }
 
 void mainUpdate() {
