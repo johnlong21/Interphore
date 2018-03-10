67a45e226e4a3c609755ded131e7d6d1455e1012
FallowWing <fallowwing@gmail.com>
2018-03-10 10:59:59 -0700

    Fixed bg sprites

--- src/writer/writer.cpp
+++ src/writer/writer.cpp
@@ -669,9 +669,7 @@ namespace Writer {
 			{ /// Bg Sprite 1
 				MintSprite *spr = createMintSprite("img/writer/mynt/bg/myntBg.png");
 				writer->bg->addChild(spr);
-				spr->setupRect(engine->width, engine->height, 0xFF0000);
 				spr->layer = lowestLayer + BG1_LAYER;
-				spr->alpha = 1;
 
 				writer->bgSprite1 = spr;
 			}
@@ -679,7 +677,6 @@ namespace Writer {
 			{ /// Bg Sprite 2
 				MintSprite *spr = createMintSprite("img/writer/mynt/bg/myntBg2.png");
 				writer->bg->addChild(spr);
-				spr->setupRect(engine->width, engine->height, 0x00FF00);
 				spr->layer = lowestLayer + BG2_LAYER;
 				spr->alpha = 0.3;
 
