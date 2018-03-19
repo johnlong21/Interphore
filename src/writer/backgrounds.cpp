3cc19ecc508acaa74e2fd82c3594df8c2470e40c
FallowWing <fallowwing@gmail.com>
2018-03-19 13:15:10 -0600

    started doing the bg scrolling

--- src/writer/backgrounds.cpp
+++ src/writer/backgrounds.cpp
@@ -2,6 +2,9 @@ namespace Writer {
 	void setBackground(int bgNum, const char *assetId);
 	void updateBackgrounds();
 
+	void setScrollingBackground(int bgNum, float scrollX, float scrollY);
+	void resetBackgroundMode(int bgNum);
+
 	void setBackground(int bgNum, const char *assetId) {
 		writer->nextBgs[bgNum] = stringClone(assetId);
 	}
@@ -31,7 +34,23 @@ namespace Writer {
 			if (writer->nextBgs[i] == NULL && writer->bgs[i]) {
 				writer->bgs[i]->alpha += 0.05;
 			}
-		}
 
+			if (writer->bgs[i]) {
+				MintSprite *bg = writer->bgs[i];
+				BackgroundMode *mode = &writer->bgModes[i];
+
+				bg->x += mode->scrollX;
+				bg->y += mode->scrollY;
+			}
+		}
+	}
+
+	void setScrollingBackground(int bgNum, float scrollX, float scrollY) {
+		writer->bgModes[bgNum].scrollX = scrollX;
+		writer->bgModes[bgNum].scrollY = scrollX;
+	}
+
+	void resetBackgroundMode(int bgNum) {
+		memset(&writer->bgModes[bgNum], 0, sizeof(BackgroundMode));
 	}
 }
