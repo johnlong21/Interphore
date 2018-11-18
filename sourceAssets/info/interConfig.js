d344e863a592ff7132d9147bcb7ad2479f995a0f
FallowWing <fallowwing@gmail.com>
2018-11-18 12:04:34 -0700

    Minor

--- sourceAssets/info/interConfig.js
+++ sourceAssets/info/interConfig.js
@@ -1100,14 +1100,16 @@ function realUpdate() {
 		spr.y = gameHeight - spr.height;
 		spr.alpha = choice.enabled ? 1 : 0.5;
 		if (spr.hovering) spr.y += 5;
-		if (prevChoices.hovering) prevChoices.y = gameHeight - prevChoices.height + 5;
-		else prevChoices.y = gameHeight - prevChoices.height;
-		if (nextChoices.hovering) nextChoices.y = gameHeight - nextChoices.height + 5
-		else nextChoices.y = gameHeight - nextChoices.height;
-		if (nextChoices.justHovered || prevChoices.justHovered) playEffect("audio/ui/hoverChoiceButtons");
-
 		choiceIndexOnPage++;
 	}
+
+	if (prevChoices.hovering) prevChoices.y = gameHeight - prevChoices.height + 5
+	else prevChoices.y = gameHeight - prevChoices.height;
+
+	if (nextChoices.hovering) nextChoices.y = gameHeight - nextChoices.height + 5
+	else nextChoices.y = gameHeight - nextChoices.height;
+
+	if (nextChoices.justHovered || prevChoices.justHovered) playEffect("audio/ui/hoverChoiceButtons");
 
 	/// Images
 	images.forEach(function(img) {
