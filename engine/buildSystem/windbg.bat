75dd2f0cc25304c53dd48f601ed6fa1c3bcae42b
Ekaterina Vaartis <vaartis@kotobank.ch>
2021-11-12 21:16:22 +0300

    Delete old buildSystem files

--- engine/buildSystem/windbg.bat
+++
@@ -1,7 +0,0 @@
-set vcroot=C:\Program Files (x86)\Microsoft Visual Studio\2017
-set sdkroot=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A
-
-pushd .
-call "%vcroot%\Community\Common7\Tools\VsDevCmd.bat"
-popd
-devenv /Run bin\engine.exe
